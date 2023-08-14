f45ca821615629f7b17d3033888d0498a22695c7
Rahul Raj <rahulrajsjs22@gmail.com>
2023-08-14 17:51:06 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': process.env.KEY,
+            'X-RapidAPI-Key': process.env.MY_KEY,
             'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
         }
     };
